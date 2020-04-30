479a0f4760a66319fb02bdd0e9aa40f6d0b0a209
Yusuf Ali <yali98319@gmail.com>
2020-04-30 16:31:02 +0530

    Updates

---
+++ precache-manifest.a5dbed89b467addb3522bf339c0eabf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686249f7a9e92674df76ab9beebd01e3",
+    "url": "/github-cards/index.html"
+  },
+  {
+    "revision": "4d7e2a47c06b67f55743",
+    "url": "/github-cards/static/css/main.5d2c59e7.chunk.css"
+  },
+  {
+    "revision": "976c75684b8288f398f6",
+    "url": "/github-cards/static/js/2.27350d8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-cards/static/js/2.27350d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7e2a47c06b67f55743",
+    "url": "/github-cards/static/js/main.20cb2aac.chunk.js"
+  },
+  {
+    "revision": "503b270a4f09fef8a378",
+    "url": "/github-cards/static/js/runtime-main.c149802c.js"
+  }
+]);
